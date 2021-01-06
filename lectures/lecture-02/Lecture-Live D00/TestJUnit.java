6ccdd86c12b9b4ee15a01c04af6b9e269920d814
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-01-05 20:31:33 -0800

    Add Live Lecture 2 files C00/D00

---
+++ lectures/lecture-02/Lecture-Live D00/TestJUnit.java
@@ -0,0 +1,19 @@
+//import static org.junit.Assert.assertEquals;
+//import org.junit.Test;
+
+public class TestJUnit {
+		
+	//@Test
+	public void testQuestion1() {
+		
+	}
+	
+	private void helper() {
+		
+	}
+	
+	public void otherHelper() {
+		
+	}
+	
+}
