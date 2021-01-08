9704f532e2f52e113b8b12e351db8fd1ef676540
yic242 <yic242@eng.ucsd.edu>
2021-01-08 09:01:08 -0800

    add annotated materials

--- lectures/lecture-03/Lecture 3 - Live - B00/src/Human.java
+++ lectures/lecture-03/Lecture 3 - Live - B00/src/Human.java
@@ -1,18 +1,3 @@
 public class Human implements Earthling{
-	String name;
-	int age;
-	public void breathe() {
-		System.out.println("breath in o2, out co2");
-	}
-	public String speak() {
-		if (age > 2) {
-			return "language";
-		}
-		else {
-			return "gibberish";
-		}
-	}
-	public boolean reproduce() {
-		return true;
-	}
+
 }
