bbff1f73a157de383e45d689e9cb335d978fa62d
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-01-11 12:50:43 -0800

    Live lecture 4 - C00/D00

---
+++ lectures/lecture-04/Lecture-Live D00/TestStringList.java
@@ -0,0 +1,73 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import java.util.*;
+
+public class TestStringList {
+
+  @Test
+  public void testAdd() {
+    StringList slist = new ArrayStringList();
+    slist.add("banana");
+    slist.add("apple");
+
+    assertEquals("banana", slist.get(0));
+    assertEquals("apple", slist.get(1));
+  }
+
+  @Test
+  public void testAddThenSize() {
+    StringList slist = new ArrayStringList();
+    slist.add("banana");
+    slist.add("apple");
+
+    assertEquals(2, slist.size());
+  }
+
+  @Test
+  public void testInsert() {
+    StringList slist = new ArrayStringList();
+    slist.insert(0, "banana");
+
+    assertEquals("banana", slist.get(0));
+
+    slist.insert(0, "apple");	//prepend
+
+    assertEquals("apple", slist.get(0));
+    assertEquals("banana", slist.get(1));
+  }
+  
+  @Test
+  public void testRemove() {
+    StringList slist = new ArrayStringList();
+    slist.insert(0, "banana");
+    slist.insert(0, "apple");	//prepend
+    slist.remove(0);
+    
+    assertEquals("banana", slist.get(0));
+    assertEquals(null, slist.get(1));
+
+    //Debugging only
+    ArrayStringList alist = (ArrayStringList) slist;
+    System.out.println(Arrays.deepToString(alist.elements));    
+  }
+
+  @Test
+  public void testAddMany() {
+    StringList slist = new ArrayStringList();
+    slist.add("a");
+    slist.add("b");
+    slist.add("c");
+    slist.add("d");
+    slist.add("e");
+
+    assertEquals("e", slist.get(4));
+    assertEquals("d", slist.get(3));
+    assertEquals("c", slist.get(2));
+    assertEquals("b", slist.get(1));
+    assertEquals("a", slist.get(0));
+
+  }
+
+
+}
+
