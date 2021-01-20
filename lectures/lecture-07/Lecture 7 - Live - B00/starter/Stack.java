4f44b6bdbc747147c7f91d6f385a35e2f8f3d56a
yic242 <yic242@eng.ucsd.edu>
2021-01-20 08:54:55 -0800

    after class Lecture 7

--- lectures/lecture-07/Lecture 7 - Live - B00/starter/Stack.java
+++ lectures/lecture-07/Lecture 7 - Live - B00/starter/Stack.java
@@ -7,12 +7,22 @@ public interface Stack<E> {
 }
 
 class ALStack<E> implements Stack<E> {
-
-
+	private ArrayList<E> contents;
 	
-	
-	
-	
+	public ALStack() {
+		contents = new ArrayList<E>();
+	}
+	public void push(E element) {
+		contents.add(element);
+		
+	}
+	public E pop() {
+		E ref = contents.remove(contents.size()-1);
+		return ref;
+	}
+	public int size() {
+		return contents.size();
+	}
 
 	public String toString() {
 		return "top -> " + this.contents.toString();
